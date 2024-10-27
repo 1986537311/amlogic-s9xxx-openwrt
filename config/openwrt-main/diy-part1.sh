4fdf0791cf65acb73bc2764a9138dd0921be935b
勿忘我 <38556221+1986537311@users.noreply.github.com>
2024-10-27 22:29:18 +0800

    Update diy-part1.sh

--- config/openwrt-main/diy-part1.sh
+++ config/openwrt-main/diy-part1.sh
@@ -7,8 +7,8 @@
 #========================================================================================================================
 
 # Add a feed source
-# sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
 
 # other
-# rm -rf package/utils/{ucode,fbtest}
+rm -rf package/utils/{ucode,fbtest}
 
